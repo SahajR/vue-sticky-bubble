0fa84af6e3fedaccd31edd649f064379109334ce
SahajR <sahaj.ramachandran@gmail.com>
2018-01-15 16:59:21 +0530

    fix: Plugin naming

--- src/lib.js
+++ src/lib.js
@@ -2,7 +2,7 @@ import vueStickyBubble from './components/vue-sticky-bubble';
 
 const vueStickyBubblePlugin = {};
 vueStickyBubblePlugin.install = (Vue) => {
-  Vue.component(vueStickyBubble.name, vueStickyBubble);
+  Vue.component('vue-sticky-bubble', vueStickyBubble);
 };
 
 export default vueStickyBubblePlugin;
